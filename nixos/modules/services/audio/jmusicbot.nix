84240ef276024d938dd023e0c663e7e4234b3595
Philip Taron <philip.taron@gmail.com>
2024-03-04 15:52:21 -0800

    nixos/jmusicbot: avoid with lib

--- nixos/modules/services/audio/jmusicbot.nix
+++ nixos/modules/services/audio/jmusicbot.nix
@@ -1,19 +1,29 @@
 { config, lib, pkgs, ... }:
 
-with lib;
 let
+  inherit (lib)
+    maintainers
+    mdDoc
+    mkEnableOption
+    mkIf
+    mkMerge
+    mkOption
+    mkPackageOption
+    types
+    ;
+
   cfg = config.services.jmusicbot;
 in
 {
   options = {
     services.jmusicbot = {
-      enable = mkEnableOption (lib.mdDoc "jmusicbot, a Discord music bot that's easy to set up and run yourself");
+      enable = mkEnableOption (mdDoc "jmusicbot, a Discord music bot that's easy to set up and run yourself");
 
       package = mkPackageOption pkgs "jmusicbot" { };
 
       stateDir = mkOption {
         type = types.path;
-        description = lib.mdDoc ''
+        description = mdDoc ''
           The directory where config.txt and serversettings.json is saved.
           If left as the default value this directory will automatically be created before JMusicBot starts, otherwise the sysadmin is responsible for ensuring the directory exists with appropriate ownership and permissions.
           Untouched by the value of this option config.txt needs to be placed manually into this directory.
