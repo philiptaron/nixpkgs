241eda041552dc8b199caaadc439b8c5a9d53a3d
Philip Taron <philip.taron@gmail.com>
2024-03-04 13:11:04 -0800

    nixos/wooting: avoid with lib

--- nixos/modules/hardware/wooting.nix
+++ nixos/modules/hardware/wooting.nix
@@ -1,8 +1,14 @@
 { config, lib, pkgs, ... }:
 
-with lib;
+let
+  inherit (lib)
+    mdDoc
+    mkEnableOption
+    mkIf
+    ;
+in
 {
-  options.hardware.wooting.enable = mkEnableOption (lib.mdDoc ''support for Wooting keyboards.
+  options.hardware.wooting.enable = mkEnableOption (mdDoc ''support for Wooting keyboards.
     Note that users must be in the "input" group for udev rules to apply'');
 
   config = mkIf config.hardware.wooting.enable {
