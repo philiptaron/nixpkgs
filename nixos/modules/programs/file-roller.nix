eb6643e83cee706440007bc303b52ba986e88959
Philip Taron <philip.taron@gmail.com>
2024-03-04 13:45:41 -0800

    nixos/file-roller: avoid with lib

--- nixos/modules/programs/file-roller.nix
+++ nixos/modules/programs/file-roller.nix
@@ -2,11 +2,18 @@
 
 { config, pkgs, lib, ... }:
 
-with lib;
+let
+  inherit (lib)
+    mdDoc
+    mkEnableOption
+    mkIf
+    mkPackageOption
+    mkRenamedOptionModule
+    ;
 
-let cfg = config.programs.file-roller;
-
-in {
+  cfg = config.programs.file-roller;
+in
+{
 
   # Added 2019-08-09
   imports = [
@@ -21,7 +28,7 @@ in {
 
     programs.file-roller = {
 
-      enable = mkEnableOption (lib.mdDoc "File Roller, an archive manager for GNOME");
+      enable = mkEnableOption (mdDoc "File Roller, an archive manager for GNOME");
 
       package = mkPackageOption pkgs [ "gnome" "file-roller" ] { };
 
