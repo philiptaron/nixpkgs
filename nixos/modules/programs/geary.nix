5a33f6f22e921eab36692cd2d37c1fdcdb1278c9
Philip Taron <philip.taron@gmail.com>
2024-03-04 13:52:02 -0800

    nixos/geary: avoid with lib

--- nixos/modules/programs/geary.nix
+++ nixos/modules/programs/geary.nix
@@ -1,17 +1,22 @@
 { config, pkgs, lib, ... }:
 
-with lib;
-
 let
-  cfg = config.programs.geary;
+  inherit (lib)
+    mdDoc
+    mkEnableOption
+    mkIf
+    teams
+    ;
 
-in {
+  cfg = config.programs.geary;
+in
+{
   meta = {
     maintainers = teams.gnome.members;
   };
 
   options = {
-    programs.geary.enable = mkEnableOption (lib.mdDoc "Geary, a Mail client for GNOME 3");
+    programs.geary.enable = mkEnableOption (mdDoc "Geary, a Mail client for GNOME 3");
   };
 
   config = mkIf cfg.enable {
