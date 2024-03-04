84914a237f03d0f28a25cdfe25e595c9e747fce7
Philip Taron <philip.taron@gmail.com>
2024-03-04 13:53:51 -0800

    nixos/hamster: avoid with lib

--- nixos/modules/programs/hamster.nix
+++ nixos/modules/programs/hamster.nix
@@ -1,14 +1,21 @@
 { config, lib, pkgs, ... }:
 
-with lib;
+let
+  inherit (lib)
+    maintainers
+    mdDoc
+    mkEnableOption
+    mkIf
+    ;
+in
 
 {
   meta.maintainers = pkgs.hamster.meta.maintainers;
 
   options.programs.hamster.enable =
-    mkEnableOption (lib.mdDoc "hamster, a time tracking program");
+    mkEnableOption (mdDoc "hamster, a time tracking program");
 
-  config = lib.mkIf config.programs.hamster.enable {
+  config = mkIf config.programs.hamster.enable {
     environment.systemPackages = [ pkgs.hamster ];
     services.dbus.packages = [ pkgs.hamster ];
   };
