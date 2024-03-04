4e57632a8f88836388ca01af8992c71c211151f8
Philip Taron <philip.taron@gmail.com>
2024-03-04 15:10:33 -0800

    nixos/kclock: avoid with lib

--- nixos/modules/programs/kclock.nix
+++ nixos/modules/programs/kclock.nix
@@ -1,10 +1,16 @@
 { lib, pkgs, config, ... }:
-with lib;
 let
+  inherit (lib)
+    mdDoc
+    mkEnableOption
+    mkIf
+    ;
+
   cfg = config.programs.kclock;
   kclockPkg = pkgs.libsForQt5.kclock;
-in {
-  options.programs.kclock = { enable = mkEnableOption (lib.mdDoc "KClock"); };
+in
+{
+  options.programs.kclock = { enable = mkEnableOption (mdDoc "KClock"); };
 
   config = mkIf cfg.enable {
     services.dbus.packages = [ kclockPkg ];
