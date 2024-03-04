ec3144c332c98f6c93043bc813eab158d5ec6151
Philip Taron <philip.taron@gmail.com>
2024-03-04 12:54:24 -0800

    nixos/ckb-next: avoid with lib

--- nixos/modules/hardware/ckb-next.nix
+++ nixos/modules/hardware/ckb-next.nix
@@ -1,46 +1,51 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    mdDoc
+    meta
+    mkEnableOption
+    mkIf
+    mkOption
+    mkPackageOption
+    mkRenamedOptionModule
+    optionalString
+    types
+    ;
+
   cfg = config.hardware.ckb-next;
-
 in
-  {
-    imports = [
-      (mkRenamedOptionModule [ "hardware" "ckb" "enable" ] [ "hardware" "ckb-next" "enable" ])
-      (mkRenamedOptionModule [ "hardware" "ckb" "package" ] [ "hardware" "ckb-next" "package" ])
-    ];
+{
+  imports = [
+    (mkRenamedOptionModule [ "hardware" "ckb" "enable" ] [ "hardware" "ckb-next" "enable" ])
+    (mkRenamedOptionModule [ "hardware" "ckb" "package" ] [ "hardware" "ckb-next" "package" ])
+  ];
 
-    options.hardware.ckb-next = {
-      enable = mkEnableOption (lib.mdDoc "the Corsair keyboard/mouse driver");
+  options.hardware.ckb-next = {
+    enable = mkEnableOption (mdDoc "the Corsair keyboard/mouse driver");
 
-      gid = mkOption {
-        type = types.nullOr types.int;
-        default = null;
-        example = 100;
-        description = lib.mdDoc ''
-          Limit access to the ckb daemon to a particular group.
-        '';
+    gid = mkOption {
+      type = types.nullOr types.int;
+      default = null;
+      example = 100;
+      description = mdDoc ''
+        Limit access to the ckb daemon to a particular group.
+      '';
+    };
+
+    package = mkPackageOption pkgs "ckb-next" { };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ cfg.package ];
+
+    systemd.services.ckb-next = {
+      description = "Corsair Keyboards and Mice Daemon";
+      wantedBy = ["multi-user.target"];
+      serviceConfig = {
+        ExecStart = "${cfg.package}/bin/ckb-next-daemon ${optionalString (cfg.gid != null) "--gid=${builtins.toString cfg.gid}"}";
+        Restart = "on-failure";
       };
-
-      package = mkPackageOption pkgs "ckb-next" { };
     };
-
-    config = mkIf cfg.enable {
-      environment.systemPackages = [ cfg.package ];
-
-      systemd.services.ckb-next = {
-        description = "Corsair Keyboards and Mice Daemon";
-        wantedBy = ["multi-user.target"];
-        serviceConfig = {
-          ExecStart = "${cfg.package}/bin/ckb-next-daemon ${optionalString (cfg.gid != null) "--gid=${builtins.toString cfg.gid}"}";
-          Restart = "on-failure";
-        };
-      };
-    };
-
-    meta = {
-      maintainers = with lib.maintainers; [ ];
-    };
-  }
+  };
+}
