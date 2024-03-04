8136f52a0e99371279351eafa4d153ddd1572b7e
Philip Taron <philip.taron@gmail.com>
2024-03-04 15:13:30 -0800

    nixos/mtr: avoid with lib

--- nixos/modules/programs/mtr.nix
+++ nixos/modules/programs/mtr.nix
@@ -1,17 +1,23 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
-  cfg = config.programs.mtr;
+  inherit (lib)
+    mdDoc
+    mkIf
+    mkOption
+    mkPackageOption
+    types
+    ;
 
-in {
+  cfg = config.programs.mtr;
+in
+{
   options = {
     programs.mtr = {
       enable = mkOption {
         type = types.bool;
         default = false;
-        description = lib.mdDoc ''
+        description = mdDoc ''
           Whether to add mtr to the global environment and configure a
           setcap wrapper for it.
         '';
