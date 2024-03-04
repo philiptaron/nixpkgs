32f0597484a9717537fb664d9ea4d0d5aaf9083d
Philip Taron <philip.taron@gmail.com>
2024-03-04 13:43:00 -0800

    nixos/criu: avoid with lib

--- nixos/modules/programs/criu.nix
+++ nixos/modules/programs/criu.nix
@@ -1,23 +1,29 @@
 { config, lib, pkgs, ... }:
 
-with lib;
+let
+  inherit (lib)
+    mdDoc
+    mkIf
+    mkOption
+    types
+    ;
 
-let cfg = config.programs.criu;
-in {
-
+  cfg = config.programs.criu;
+in
+{
   options = {
     programs.criu = {
       enable = mkOption {
         type = types.bool;
         default = false;
-        description = lib.mdDoc ''
+        description = mdDoc ''
           Install {command}`criu` along with necessary kernel options.
         '';
       };
     };
   };
   config = mkIf cfg.enable {
-    system.requiredKernelConfig = with config.lib.kernelConfig; [
+    system.requiredKernelConfig = with config.kernelConfig; [
       (isYes "CHECKPOINT_RESTORE")
     ];
     boot.kernel.features.criu = true;
