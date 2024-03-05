acf0028f991c7e11bf7cd5fca3e02f3a0f355f8b
Philip Taron <philip.taron@gmail.com>
2024-03-05 11:02:43 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/networking/keepalived/vrrp-script-options.nix

--- nixos/modules/services/networking/keepalived/vrrp-script-options.nix
+++ nixos/modules/services/networking/keepalived/vrrp-script-options.nix
@@ -1,62 +1,68 @@
 { lib } :
 
-with lib;
-with lib.types;
+let
+  inherit (lib)
+    literalExpression
+    mdDoc
+    mkOption
+    types
+    ;
+in
 {
   options = {
 
     script = mkOption {
-      type = str;
+      type = types.str;
       example = literalExpression ''"''${pkgs.curl} -f http://localhost:80"'';
-      description = lib.mdDoc "(Path of) Script command to execute followed by args, i.e. cmd [args]...";
+      description = mdDoc "(Path of) Script command to execute followed by args, i.e. cmd [args]...";
     };
 
     interval = mkOption {
-      type = int;
+      type = types.int;
       default = 1;
-      description = lib.mdDoc "Seconds between script invocations.";
+      description = mdDoc "Seconds between script invocations.";
     };
 
     timeout = mkOption {
-      type = int;
+      type = types.int;
       default = 5;
-      description = lib.mdDoc "Seconds after which script is considered to have failed.";
+      description = mdDoc "Seconds after which script is considered to have failed.";
     };
 
     weight = mkOption {
-      type = int;
+      type = types.int;
       default = 0;
-      description = lib.mdDoc "Following a failure, adjust the priority by this weight.";
+      description = mdDoc "Following a failure, adjust the priority by this weight.";
     };
 
     rise = mkOption {
-      type = int;
+      type = types.int;
       default = 5;
-      description = lib.mdDoc "Required number of successes for OK transition.";
+      description = mdDoc "Required number of successes for OK transition.";
     };
 
     fall = mkOption {
-      type = int;
+      type = types.int;
       default = 3;
-      description = lib.mdDoc "Required number of failures for KO transition.";
+      description = mdDoc "Required number of failures for KO transition.";
     };
 
     user = mkOption {
-      type = str;
+      type = types.str;
       default = "keepalived_script";
-      description = lib.mdDoc "Name of user to run the script under.";
+      description = mdDoc "Name of user to run the script under.";
     };
 
     group = mkOption {
-      type = nullOr str;
+      type = types.nullOr types.str;
       default = null;
-      description = lib.mdDoc "Name of group to run the script under. Defaults to user group.";
+      description = mdDoc "Name of group to run the script under. Defaults to user group.";
     };
 
     extraConfig = mkOption {
-      type = lines;
+      type = types.lines;
       default = "";
-      description = lib.mdDoc "Extra lines to be added verbatim to the vrrp_script section.";
+      description = mdDoc "Extra lines to be added verbatim to the vrrp_script section.";
     };
 
   };
