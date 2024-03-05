5aaefc130133b812f58a36f93b33dfcff7d92d06
Philip Taron <philip.taron@gmail.com>
2024-03-04 20:59:05 -0800

    nixos/confd: avoid with lib

--- nixos/modules/services/misc/confd.nix
+++ nixos/modules/services/misc/confd.nix
@@ -1,8 +1,18 @@
 { config, pkgs, lib, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    boolToString
+    concatMapStringsSep
+    mdDoc
+    mkDefault
+    mkEnableOption
+    mkIf
+    mkOption
+    mkPackageOption
+    types
+    ;
+
   cfg = config.services.confd;
 
   confdConfig = ''
@@ -15,48 +25,49 @@ let
     watch = ${boolToString cfg.watch}
   '';
 
-in {
+in
+{
   options.services.confd = {
-    enable = mkEnableOption (lib.mdDoc "confd service");
+    enable = mkEnableOption (mdDoc "confd service");
 
     backend = mkOption {
-      description = lib.mdDoc "Confd config storage backend to use.";
+      description = mdDoc "Confd config storage backend to use.";
       default = "etcd";
       type = types.enum ["etcd" "consul" "redis" "zookeeper"];
     };
 
     interval = mkOption {
-      description = lib.mdDoc "Confd check interval.";
+      description = mdDoc "Confd check interval.";
       default = 10;
       type = types.int;
     };
 
     nodes = mkOption {
-      description = lib.mdDoc "Confd list of nodes to connect to.";
+      description = mdDoc "Confd list of nodes to connect to.";
       default = [ "http://127.0.0.1:2379" ];
       type = types.listOf types.str;
     };
 
     watch = mkOption {
-      description = lib.mdDoc "Confd, whether to watch etcd config for changes.";
+      description = mdDoc "Confd, whether to watch etcd config for changes.";
       default = true;
       type = types.bool;
     };
 
     prefix = mkOption {
-      description = lib.mdDoc "The string to prefix to keys.";
+      description = mdDoc "The string to prefix to keys.";
       default = "/";
       type = types.path;
     };
 
     logLevel = mkOption {
-      description = lib.mdDoc "Confd log level.";
+      description = mdDoc "Confd log level.";
       default = "info";
       type = types.enum ["info" "debug"];
     };
 
     confDir = mkOption {
-      description = lib.mdDoc "The path to the confd configs.";
+      description = mdDoc "The path to the confd configs.";
       default = "/etc/confd";
       type = types.path;
     };
