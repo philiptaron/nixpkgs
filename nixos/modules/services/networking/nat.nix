aae0291b228f5ce0a8bdd0ba3abe47c97651e849
Philip Taron <philip.taron@gmail.com>
2024-03-05 11:10:49 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/networking/nat.nix

--- nixos/modules/services/networking/nat.nix
+++ nixos/modules/services/networking/nat.nix
@@ -4,9 +4,16 @@
 
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    literalExpression
+    mdDoc
+    mkIf
+    mkOption
+    mkOverride
+    optionalAttrs
+    types
+    ;
 
   cfg = config.networking.nat;
 
@@ -19,7 +26,7 @@ in
     networking.nat.enable = mkOption {
       type = types.bool;
       default = false;
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Whether to enable Network Address Translation (NAT).
       '';
     };
@@ -27,7 +34,7 @@ in
     networking.nat.enableIPv6 = mkOption {
       type = types.bool;
       default = false;
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Whether to enable IPv6 NAT.
       '';
     };
@@ -36,7 +43,7 @@ in
       type = types.listOf types.str;
       default = [ ];
       example = [ "eth0" ];
-      description = lib.mdDoc ''
+      description = mdDoc ''
         The interfaces for which to perform NAT. Packets coming from
         these interface and destined for the external interface will
         be rewritten.
@@ -47,7 +54,7 @@ in
       type = types.listOf types.str;
       default = [ ];
       example = [ "192.168.1.0/24" ];
-      description = lib.mdDoc ''
+      description = mdDoc ''
         The IP address ranges for which to perform NAT.  Packets
         coming from these addresses (on any interface) and destined
         for the external interface will be rewritten.
@@ -58,7 +65,7 @@ in
       type = types.listOf types.str;
       default = [ ];
       example = [ "fc00::/64" ];
-      description = lib.mdDoc ''
+      description = mdDoc ''
         The IPv6 address ranges for which to perform NAT.  Packets
         coming from these addresses (on any interface) and destined
         for the external interface will be rewritten.
@@ -69,7 +76,7 @@ in
       type = types.nullOr types.str;
       default = null;
       example = "eth1";
-      description = lib.mdDoc ''
+      description = mdDoc ''
         The name of the external network interface.
       '';
     };
@@ -78,7 +85,7 @@ in
       type = types.nullOr types.str;
       default = null;
       example = "203.0.113.123";
-      description = lib.mdDoc ''
+      description = mdDoc ''
         The public IP address to which packets from the local
         network are to be rewritten.  If this is left empty, the
         IP address associated with the external interface will be
@@ -90,7 +97,7 @@ in
       type = types.nullOr types.str;
       default = null;
       example = "2001:dc0:2001:11::175";
-      description = lib.mdDoc ''
+      description = mdDoc ''
         The public IPv6 address to which packets from the local
         network are to be rewritten.  If this is left empty, the
         IP address associated with the external interface will be
@@ -104,27 +111,27 @@ in
           sourcePort = mkOption {
             type = types.either types.int (types.strMatching "[[:digit:]]+:[[:digit:]]+");
             example = 8080;
-            description = lib.mdDoc "Source port of the external interface; to specify a port range, use a string with a colon (e.g. \"60000:61000\")";
+            description = mdDoc "Source port of the external interface; to specify a port range, use a string with a colon (e.g. \"60000:61000\")";
           };
 
           destination = mkOption {
             type = types.str;
             example = "10.0.0.1:80";
-            description = lib.mdDoc "Forward connection to destination ip:port (or [ipv6]:port); to specify a port range, use ip:start-end";
+            description = mdDoc "Forward connection to destination ip:port (or [ipv6]:port); to specify a port range, use ip:start-end";
           };
 
           proto = mkOption {
             type = types.str;
             default = "tcp";
             example = "udp";
-            description = lib.mdDoc "Protocol of forwarded connection";
+            description = mdDoc "Protocol of forwarded connection";
           };
 
           loopbackIPs = mkOption {
             type = types.listOf types.str;
             default = [ ];
             example = literalExpression ''[ "55.1.2.3" ]'';
-            description = lib.mdDoc "Public IPs for NAT reflection; for connections to `loopbackip:sourcePort` from the host itself and from other hosts behind NAT";
+            description = mdDoc "Public IPs for NAT reflection; for connections to `loopbackip:sourcePort` from the host itself and from other hosts behind NAT";
           };
         };
       });
@@ -133,7 +140,7 @@ in
         { sourcePort = 8080; destination = "10.0.0.1:80"; proto = "tcp"; }
         { sourcePort = 8080; destination = "[fc00::2]:80"; proto = "tcp"; }
       ];
-      description = lib.mdDoc ''
+      description = mdDoc ''
         List of forwarded ports from the external interface to
         internal destinations by using DNAT. Destination can be
         IPv6 if IPv6 NAT is enabled.
@@ -144,7 +151,7 @@ in
       type = types.nullOr types.str;
       default = null;
       example = "10.0.0.1";
-      description = lib.mdDoc ''
+      description = mdDoc ''
         The local IP address to which all traffic that does not match any
         forwarding rule is forwarded.
       '';
