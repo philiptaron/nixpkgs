a02d0a152e43e54e0e1795e527f6a19d6ef3c504
Philip Taron <philip.taron@gmail.com>
2024-03-04 21:12:19 -0800

    nixos/input-remapper: avoid with lib

--- nixos/modules/services/misc/input-remapper.nix
+++ nixos/modules/services/misc/input-remapper.nix
@@ -1,19 +1,29 @@
 { pkgs, lib, config, ... }:
 
-with lib;
+let
+  inherit (lib)
+    maintainers
+    mdDoc
+    mkEnableOption
+    mkIf
+    mkOption
+    mkPackageOption
+    types
+    ;
 
-let cfg = config.services.input-remapper; in
+  cfg = config.services.input-remapper;
+in
 {
   options = {
     services.input-remapper = {
-      enable = mkEnableOption (lib.mdDoc "input-remapper, an easy to use tool to change the mapping of your input device buttons");
+      enable = mkEnableOption (mdDoc "input-remapper, an easy to use tool to change the mapping of your input device buttons");
       package = mkPackageOption pkgs "input-remapper" { };
-      enableUdevRules = mkEnableOption (lib.mdDoc "udev rules added by input-remapper to handle hotplugged devices. Currently disabled by default due to https://github.com/sezanzeb/input-remapper/issues/140");
+      enableUdevRules = mkEnableOption (mdDoc "udev rules added by input-remapper to handle hotplugged devices. Currently disabled by default due to https://github.com/sezanzeb/input-remapper/issues/140");
       serviceWantedBy = mkOption {
         default = [ "graphical.target" ];
         example = [ "multi-user.target" ];
         type = types.listOf types.str;
-        description = lib.mdDoc "Specifies the WantedBy setting for the input-remapper service.";
+        description = mdDoc "Specifies the WantedBy setting for the input-remapper service.";
       };
     };
   };
@@ -26,5 +36,5 @@ let cfg = config.services.input-remapper; in
     systemd.services.input-remapper.wantedBy = cfg.serviceWantedBy;
   };
 
-  meta.maintainers = with lib.maintainers; [ LunNova ];
+  meta.maintainers = with maintainers; [ LunNova ];
 }
