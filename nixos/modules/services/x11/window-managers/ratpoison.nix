a954f402bddba5b2498d02a4267f8936c7054a56
Philip Taron <philip.taron@gmail.com>
2024-03-05 14:02:30 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/x11/window-managers/ratpoison.nix

--- nixos/modules/services/x11/window-managers/ratpoison.nix
+++ nixos/modules/services/x11/window-managers/ratpoison.nix
@@ -1,14 +1,19 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    mdDoc
+    mkEnableOption
+    mkIf
+    singleton
+    ;
+
   cfg = config.services.xserver.windowManager.ratpoison;
 in
 {
   ###### interface
   options = {
-    services.xserver.windowManager.ratpoison.enable = mkEnableOption (lib.mdDoc "ratpoison");
+    services.xserver.windowManager.ratpoison.enable = mkEnableOption (mdDoc "ratpoison");
   };
 
   ###### implementation
