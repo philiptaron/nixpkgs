75c9bdf7bae0890dbefff8d3b06aa31d15be0d71
Philip Taron <philip.taron@gmail.com>
2024-03-05 09:43:23 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/monitoring/prometheus/exporters/flow.nix

--- nixos/modules/services/monitoring/prometheus/exporters/flow.nix
+++ nixos/modules/services/monitoring/prometheus/exporters/flow.nix
@@ -1,8 +1,15 @@
 { config, lib, pkgs, options }:
 
-with lib;
-
 let
+  inherit (lib)
+    concatStringsSep
+    literalExpression
+    mdDoc
+    mkOption
+    optionalString
+    types
+    ;
+
   cfg = config.services.prometheus.exporters.flow;
 in {
   port = 9590;
@@ -10,19 +17,19 @@ in {
     brokers = mkOption {
       type = types.listOf types.str;
       example = literalExpression ''[ "kafka.example.org:19092" ]'';
-      description = lib.mdDoc "List of Kafka brokers to connect to.";
+      description = mdDoc "List of Kafka brokers to connect to.";
     };
 
     asn = mkOption {
       type = types.ints.positive;
       example = 65542;
-      description = lib.mdDoc "The ASN being monitored.";
+      description = mdDoc "The ASN being monitored.";
     };
 
     partitions = mkOption {
       type = types.listOf types.int;
       default = [];
-      description = lib.mdDoc ''
+      description = mdDoc ''
         The number of the partitions to consume, none means all.
       '';
     };
@@ -30,7 +37,7 @@ in {
     topic = mkOption {
       type = types.str;
       example = "pmacct.acct";
-      description = lib.mdDoc "The Kafka topic to consume from.";
+      description = mdDoc "The Kafka topic to consume from.";
     };
   };
 
