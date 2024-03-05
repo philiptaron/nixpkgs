5ba5c47d171d8dca7d1b396d4fde39c6c1f0d2d5
Philip Taron <philip.taron@gmail.com>
2024-03-05 09:47:30 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/monitoring/prometheus/exporters/nginx.nix

--- nixos/modules/services/monitoring/prometheus/exporters/nginx.nix
+++ nixos/modules/services/monitoring/prometheus/exporters/nginx.nix
@@ -1,8 +1,18 @@
 { config, lib, pkgs, options }:
 
-with lib;
-
 let
+  inherit (lib)
+    concatStringsSep
+    mdDoc
+    mkIf
+    mkMerge
+    mkOption
+    mkRemovedOptionModule
+    mkRenamedOptionModule
+    optionalString
+    types
+    ;
+
   cfg = config.services.prometheus.exporters.nginx;
 in
 {
@@ -11,7 +21,7 @@ in
     scrapeUri = mkOption {
       type = types.str;
       default = "http://localhost/nginx_status";
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Address to access the nginx status page.
         Can be enabled with services.nginx.statusPage = true.
       '';
@@ -19,14 +29,14 @@ in
     telemetryPath = mkOption {
       type = types.str;
       default = "/metrics";
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Path under which to expose metrics.
       '';
     };
     sslVerify = mkOption {
       type = types.bool;
       default = true;
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Whether to perform certificate verification for https.
       '';
     };
@@ -37,7 +47,7 @@ in
         "label1=value1"
         "label2=value2"
       ];
-      description = lib.mdDoc ''
+      description = mdDoc ''
         A list of constant labels that will be used in every metric.
       '';
     };
@@ -48,7 +58,7 @@ in
       ExecStart = ''
         ${pkgs.prometheus-nginx-exporter}/bin/nginx-prometheus-exporter \
           --nginx.scrape-uri='${cfg.scrapeUri}' \
-          --${lib.optionalString (!cfg.sslVerify) "no-"}nginx.ssl-verify \
+          --${optionalString (!cfg.sslVerify) "no-"}nginx.ssl-verify \
           --web.listen-address=${cfg.listenAddress}:${toString cfg.port} \
           --web.telemetry-path=${cfg.telemetryPath} \
           ${concatStringsSep " \\\n  " cfg.extraFlags}
