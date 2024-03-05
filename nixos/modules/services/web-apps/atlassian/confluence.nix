783dfbd807f6a2659da86d5b9317d6e3ed424b7d
Philip Taron <philip.taron@gmail.com>
2024-03-05 13:03:18 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/web-apps/atlassian/confluence.nix

--- nixos/modules/services/web-apps/atlassian/confluence.nix
+++ nixos/modules/services/web-apps/atlassian/confluence.nix
@@ -1,8 +1,17 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    concatStringsSep
+    mdDoc
+    mkEnableOption
+    mkIf
+    mkOption
+    mkPackageOption
+    optionalAttrs
+    optionalString
+    types
+    ;
 
   cfg = config.services.confluence;
 
@@ -29,101 +38,101 @@ in
 {
   options = {
     services.confluence = {
-      enable = mkEnableOption (lib.mdDoc "Atlassian Confluence service");
+      enable = mkEnableOption (mdDoc "Atlassian Confluence service");
 
       user = mkOption {
         type = types.str;
         default = "confluence";
-        description = lib.mdDoc "User which runs confluence.";
+        description = mdDoc "User which runs confluence.";
       };
 
       group = mkOption {
         type = types.str;
         default = "confluence";
-        description = lib.mdDoc "Group which runs confluence.";
+        description = mdDoc "Group which runs confluence.";
       };
 
       home = mkOption {
         type = types.str;
         default = "/var/lib/confluence";
-        description = lib.mdDoc "Home directory of the confluence instance.";
+        description = mdDoc "Home directory of the confluence instance.";
       };
 
       listenAddress = mkOption {
         type = types.str;
         default = "127.0.0.1";
-        description = lib.mdDoc "Address to listen on.";
+        description = mdDoc "Address to listen on.";
       };
 
       listenPort = mkOption {
         type = types.port;
         default = 8090;
-        description = lib.mdDoc "Port to listen on.";
+        description = mdDoc "Port to listen on.";
       };
 
       catalinaOptions = mkOption {
         type = types.listOf types.str;
         default = [];
         example = [ "-Xms1024m" "-Xmx2048m" "-Dconfluence.disable.peopledirectory.all=true" ];
-        description = lib.mdDoc "Java options to pass to catalina/tomcat.";
+        description = mdDoc "Java options to pass to catalina/tomcat.";
       };
 
       proxy = {
-        enable = mkEnableOption (lib.mdDoc "proxy support");
+        enable = mkEnableOption (mdDoc "proxy support");
 
         name = mkOption {
           type = types.str;
           example = "confluence.example.com";
-          description = lib.mdDoc "Virtual hostname at the proxy";
+          description = mdDoc "Virtual hostname at the proxy";
         };
 
         port = mkOption {
           type = types.port;
           default = 443;
           example = 80;
-          description = lib.mdDoc "Port used at the proxy";
+          description = mdDoc "Port used at the proxy";
         };
 
         scheme = mkOption {
           type = types.str;
           default = "https";
           example = "http";
-          description = lib.mdDoc "Protocol used at the proxy.";
+          description = mdDoc "Protocol used at the proxy.";
         };
       };
 
       sso = {
-        enable = mkEnableOption (lib.mdDoc "SSO with Atlassian Crowd");
+        enable = mkEnableOption (mdDoc "SSO with Atlassian Crowd");
 
         crowd = mkOption {
           type = types.str;
           example = "http://localhost:8095/crowd";
-          description = lib.mdDoc "Crowd Base URL without trailing slash";
+          description = mdDoc "Crowd Base URL without trailing slash";
         };
 
         applicationName = mkOption {
           type = types.str;
           example = "jira";
-          description = lib.mdDoc "Exact name of this Confluence instance in Crowd";
+          description = mdDoc "Exact name of this Confluence instance in Crowd";
         };
 
         applicationPassword = mkOption {
           type = types.nullOr types.str;
           default = null;
-          description = lib.mdDoc "Application password of this Confluence instance in Crowd";
+          description = mdDoc "Application password of this Confluence instance in Crowd";
         };
 
         applicationPasswordFile = mkOption {
           type = types.nullOr types.str;
           default = null;
-          description = lib.mdDoc "Path to the application password for Crowd of Confluence.";
+          description = mdDoc "Path to the application password for Crowd of Confluence.";
         };
 
         validationInterval = mkOption {
           type = types.int;
           default = 2;
           example = 0;
-          description = lib.mdDoc ''
+          description = mdDoc ''
             Set to 0, if you want authentication checks to occur on each
             request. Otherwise set to the number of minutes between request
             to validate if the user is logged in or out of the Crowd SSO
@@ -194,7 +203,7 @@ in
         mkdir -p ${cfg.home}/{logs,work,temp,deploy}
 
         sed -e 's,port="8090",port="${toString cfg.listenPort}" address="${cfg.listenAddress}",' \
-        '' + (lib.optionalString cfg.proxy.enable ''
+        '' + (optionalString cfg.proxy.enable ''
           -e 's,protocol="org.apache.coyote.http11.Http11NioProtocol",protocol="org.apache.coyote.http11.Http11NioProtocol" proxyName="${cfg.proxy.name}" proxyPort="${toString cfg.proxy.port}" scheme="${cfg.proxy.scheme}",' \
         '') + ''
           ${pkg}/conf/server.xml.dist > ${cfg.home}/server.xml
