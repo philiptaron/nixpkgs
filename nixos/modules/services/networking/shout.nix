94676591f0ca772831280b880ef47d220490d72a
Philip Taron <philip.taron@gmail.com>
2024-03-05 11:37:03 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/networking/shout.nix

--- nixos/modules/services/networking/shout.nix
+++ nixos/modules/services/networking/shout.nix
@@ -1,8 +1,15 @@
 { pkgs, lib, config, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    concatStringsSep
+    mdDoc
+    mkEnableOption
+    mkIf
+    mkOption
+    types
+    ;
+
   cfg = config.services.shout;
   shoutHome = "/var/lib/shout";
 
@@ -23,12 +30,12 @@ let
 
 in {
   options.services.shout = {
-    enable = mkEnableOption (lib.mdDoc "Shout web IRC client");
+    enable = mkEnableOption (mdDoc "Shout web IRC client");
 
     private = mkOption {
       type = types.bool;
       default = false;
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Make your shout instance private. You will need to configure user
         accounts by adding entries in {file}`${shoutHome}/users`.
       '';
@@ -37,19 +44,19 @@ in {
     listenAddress = mkOption {
       type = types.str;
       default = "0.0.0.0";
-      description = lib.mdDoc "IP interface to listen on for http connections.";
+      description = mdDoc "IP interface to listen on for http connections.";
     };
 
     port = mkOption {
       type = types.port;
       default = 9000;
-      description = lib.mdDoc "TCP port to listen on for http connections.";
+      description = mdDoc "TCP port to listen on for http connections.";
     };
 
     configFile = mkOption {
       type = types.nullOr types.lines;
       default = null;
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Contents of Shout's {file}`config.js` file.
 
         Used for backward compatibility, recommended way is now to use
@@ -70,7 +77,7 @@ in {
           port = 6697;
         };
       };
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Shout {file}`config.js` contents as attribute set (will be
         converted to JSON to generate the configuration file).
 
