ea9557db165f744f41ecb2a292d550b072d1eb99
Philip Taron <philip.taron@gmail.com>
2024-03-05 15:04:41 -0800

    Avoid `with lib;` at the top level in nixos/modules/system/etc/etc.nix

--- nixos/modules/system/etc/etc.nix
+++ nixos/modules/system/etc/etc.nix
@@ -2,9 +2,23 @@
 
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    attrValues
+    concatMapStringsSep
+    concatStringsSep
+    escapeShellArgs
+    filter
+    literalExpression
+    mdDoc
+    mkDefault
+    mkDerivedConfig
+    mkIf
+    mkOption
+    optionals
+    replaceStrings
+    types
+    ;
 
   etc' = filter (f: f.enable) (attrValues config.environment.etc);
 
@@ -86,7 +100,7 @@ in
       enable = mkOption {
         type = types.bool;
         default = false;
-        description = lib.mdDoc ''
+        description = mdDoc ''
           Mount `/etc` as an overlayfs instead of generating it via a perl script.
 
           Note: This is currently experimental. Only enable this option if you're
@@ -97,7 +111,7 @@ in
       mutable = mkOption {
         type = types.bool;
         default = true;
-        description = lib.mdDoc ''
+        description = mdDoc ''
           Whether to mount `/etc` mutably (i.e. read-write) or immutably (i.e. read-only).
 
           If this is false, only the immutable lowerdir is mounted. If it is
@@ -116,7 +130,7 @@ in
           "default/useradd".text = "GROUP=100 ...";
         }
       '';
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Set of files that have to be linked in {file}`/etc`.
       '';
 
@@ -127,7 +141,7 @@ in
             enable = mkOption {
               type = types.bool;
               default = true;
-              description = lib.mdDoc ''
+              description = mdDoc ''
                 Whether this /etc file should be generated.  This
                 option allows specific /etc files to be disabled.
               '';
@@ -135,7 +149,7 @@ in
 
             target = mkOption {
               type = types.str;
-              description = lib.mdDoc ''
+              description = mdDoc ''
                 Name of symlink (relative to
                 {file}`/etc`).  Defaults to the attribute
                 name.
@@ -145,19 +159,19 @@ in
             text = mkOption {
               default = null;
               type = types.nullOr types.lines;
-              description = lib.mdDoc "Text of the file.";
+              description = mdDoc "Text of the file.";
             };
 
             source = mkOption {
               type = types.path;
-              description = lib.mdDoc "Path of the source file.";
+              description = mdDoc "Path of the source file.";
             };
 
             mode = mkOption {
               type = types.str;
               default = "symlink";
               example = "0600";
-              description = lib.mdDoc ''
+              description = mdDoc ''
                 If set to something else than `symlink`,
                 the file is copied instead of symlinked, with the given
                 file mode.
@@ -167,7 +181,7 @@ in
             uid = mkOption {
               default = 0;
               type = types.int;
-              description = lib.mdDoc ''
+              description = mdDoc ''
                 UID of created file. Only takes effect when the file is
                 copied (that is, the mode is not 'symlink').
                 '';
@@ -176,7 +190,7 @@ in
             gid = mkOption {
               default = 0;
               type = types.int;
-              description = lib.mdDoc ''
+              description = mdDoc ''
                 GID of created file. Only takes effect when the file is
                 copied (that is, the mode is not 'symlink').
               '';
@@ -185,7 +199,7 @@ in
             user = mkOption {
               default = "+${toString config.uid}";
               type = types.str;
-              description = lib.mdDoc ''
+              description = mdDoc ''
                 User name of created file.
                 Only takes effect when the file is copied (that is, the mode is not 'symlink').
                 Changing this option takes precedence over `uid`.
@@ -195,7 +209,7 @@ in
             group = mkOption {
               default = "+${toString config.gid}";
               type = types.str;
-              description = lib.mdDoc ''
+              description = mdDoc ''
                 Group name of created file.
                 Only takes effect when the file is copied (that is, the mode is not 'symlink').
                 Changing this option takes precedence over `gid`.
@@ -207,7 +221,7 @@ in
           config = {
             target = mkDefault name;
             source = mkIf (config.text != null) (
-              let name' = "etc-" + lib.replaceStrings ["/"] ["-"] name;
+              let name' = "etc-" + replaceStrings ["/"] ["-"] name;
               in mkDerivedConfig options.text (pkgs.writeText name')
             );
           };
@@ -225,11 +239,11 @@ in
 
     system.build.etc = etc;
     system.build.etcActivationCommands = let
-      etcOverlayOptions = lib.concatStringsSep "," ([
+      etcOverlayOptions = concatStringsSep "," ([
         "relatime"
         "redirect_dir=on"
         "metacopy=on"
-      ] ++ lib.optionals config.system.etc.overlay.mutable [
+      ] ++ optionals config.system.etc.overlay.mutable [
         "upperdir=/.rw-etc/upper"
         "workdir=/.rw-etc/work"
       ]);
