d3c9142989421297b142adc14338d3162c311c32
Philip Taron <philip.taron@gmail.com>
2024-03-05 12:11:19 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/networking/toxvpn.nix

--- nixos/modules/services/networking/toxvpn.nix
+++ nixos/modules/services/networking/toxvpn.nix
@@ -1,29 +1,38 @@
 { config, pkgs, lib, ... }:
 
-with lib;
+let
+  inherit (lib)
+    concatMapStringsSep
+    mdDoc
+    mkEnableOption
+    mkIf
+    mkOption
+    types
+    ;
+in
 
 {
   options = {
     services.toxvpn = {
-      enable = mkEnableOption (lib.mdDoc "toxvpn running on startup");
+      enable = mkEnableOption (mdDoc "toxvpn running on startup");
 
       localip = mkOption {
         type        = types.str;
         default     = "10.123.123.1";
-        description = lib.mdDoc "your ip on the vpn";
+        description = mdDoc "your ip on the vpn";
       };
 
       port = mkOption {
         type        = types.port;
         default     = 33445;
-        description = lib.mdDoc "udp port for toxcore, port-forward to help with connectivity if you run many nodes behind one NAT";
+        description = mdDoc "udp port for toxcore, port-forward to help with connectivity if you run many nodes behind one NAT";
       };
 
       auto_add_peers = mkOption {
         type        = types.listOf types.str;
         default     = [];
         example     = [ "toxid1" "toxid2" ];
-        description = lib.mdDoc "peers to automatically connect to on startup";
+        description = mdDoc "peers to automatically connect to on startup";
       };
     };
   };
@@ -43,7 +52,7 @@ with lib;
       path = [ pkgs.toxvpn ];
 
       script = ''
-        exec toxvpn -i ${config.services.toxvpn.localip} -l /run/toxvpn/control -u toxvpn -p ${toString config.services.toxvpn.port} ${lib.concatMapStringsSep " " (x: "-a ${x}") config.services.toxvpn.auto_add_peers}
+        exec toxvpn -i ${config.services.toxvpn.localip} -l /run/toxvpn/control -u toxvpn -p ${toString config.services.toxvpn.port} ${concatMapStringsSep " " (x: "-a ${x}") config.services.toxvpn.auto_add_peers}
       '';
 
       serviceConfig = {
