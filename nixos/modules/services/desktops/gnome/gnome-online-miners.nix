0036cdb434ab3ce667e5c60293b9732565b23da2
Philip Taron <philip.taron@gmail.com>
2024-03-04 17:09:14 -0800

    nixos/gnome-online-miners: avoid with lib

--- nixos/modules/services/desktops/gnome/gnome-online-miners.nix
+++ nixos/modules/services/desktops/gnome/gnome-online-miners.nix
@@ -2,7 +2,16 @@
 
 { config, pkgs, lib, ... }:
 
-with lib;
+let
+  inherit (lib)
+    mdDoc
+    mkIf
+    mkOption
+    mkRenamedOptionModule
+    teams
+    types
+    ;
+in
 
 {
 
@@ -27,7 +36,7 @@ with lib;
       enable = mkOption {
         type = types.bool;
         default = false;
-        description = lib.mdDoc ''
+        description = mdDoc ''
           Whether to enable GNOME Online Miners, a service that
           crawls through your online content.
         '';
