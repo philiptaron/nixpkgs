a5400f8746b8c1bd3688ac2446051469392ff813
Philip Taron <philip.taron@gmail.com>
2024-03-05 14:02:36 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/x11/window-managers/smallwm.nix

--- nixos/modules/services/x11/window-managers/smallwm.nix
+++ nixos/modules/services/x11/window-managers/smallwm.nix
@@ -1,14 +1,19 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    mdDoc
+    mkEnableOption
+    mkIf
+    singleton
+    ;
+
   cfg = config.services.xserver.windowManager.smallwm;
 in
 {
   ###### interface
   options = {
-    services.xserver.windowManager.smallwm.enable = mkEnableOption (lib.mdDoc "smallwm");
+    services.xserver.windowManager.smallwm.enable = mkEnableOption (mdDoc "smallwm");
   };
 
   ###### implementation
