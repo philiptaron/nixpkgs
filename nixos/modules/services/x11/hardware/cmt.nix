14872df36af3187a84a3993aef626b2f2435a419
Philip Taron <philip.taron@gmail.com>
2024-03-05 13:54:41 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/x11/hardware/cmt.nix

--- nixos/modules/services/x11/hardware/cmt.nix
+++ nixos/modules/services/x11/hardware/cmt.nix
@@ -1,13 +1,19 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    mdDoc
+    mkIf
+    mkOption
+    types
+    ;
 
-cfg = config.services.xserver.cmt;
-etcPath = "X11/xorg.conf.d";
+  cfg = config.services.xserver.cmt;
 
-in {
+  etcPath = "X11/xorg.conf.d";
+
+in
+{
 
   options = {
 
@@ -15,12 +21,12 @@ in {
       enable = mkOption {
         type = types.bool;
         default = false;
-        description = lib.mdDoc "Enable chrome multitouch input (cmt). Touchpad drivers that are configured for chromebooks.";
+        description = mdDoc "Enable chrome multitouch input (cmt). Touchpad drivers that are configured for chromebooks.";
       };
       models = mkOption {
         type = types.enum [ "atlas" "banjo" "candy" "caroline" "cave" "celes" "clapper" "cyan" "daisy" "elan" "elm" "enguarde" "eve" "expresso" "falco" "gandof" "glimmer" "gnawty" "heli" "kevin" "kip" "leon" "lulu" "orco" "pbody" "peppy" "pi" "pit" "puppy" "quawks" "rambi" "samus" "snappy" "spring" "squawks" "swanky" "winky" "wolf" "auron_paine" "auron_yuna" "daisy_skate" "nyan_big" "nyan_blaze" "veyron_jaq" "veyron_jerry" "veyron_mighty" "veyron_minnie" "veyron_speedy" ];
         example = "banjo";
-        description = lib.mdDoc ''
+        description = mdDoc ''
           Which models to enable cmt for. Enter the Code Name for your Chromebook.
           Code Name can be found at <https://www.chromium.org/chromium-os/developer-information-for-chrome-os-devices>.
         '';
