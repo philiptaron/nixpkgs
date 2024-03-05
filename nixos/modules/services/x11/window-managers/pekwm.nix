63700ea2c77fd001c5ad5c0e47310e5ac03f2eca
Philip Taron <philip.taron@gmail.com>
2024-03-05 14:02:02 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/x11/window-managers/pekwm.nix

--- nixos/modules/services/x11/window-managers/pekwm.nix
+++ nixos/modules/services/x11/window-managers/pekwm.nix
@@ -1,14 +1,19 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    mdDoc
+    mkEnableOption
+    mkIf
+    singleton
+    ;
+
   cfg = config.services.xserver.windowManager.pekwm;
 in
 {
   ###### interface
   options = {
-    services.xserver.windowManager.pekwm.enable = mkEnableOption (lib.mdDoc "pekwm");
+    services.xserver.windowManager.pekwm.enable = mkEnableOption (mdDoc "pekwm");
   };
 
   ###### implementation
