6e100487f73ac8c2806de050e4156ea755901897
Philip Taron <philip.taron@gmail.com>
2024-03-05 13:21:14 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/web-apps/node-red.nix

--- nixos/modules/services/web-apps/node-red.nix
+++ nixos/modules/services/web-apps/node-red.nix
@@ -1,8 +1,22 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    concatStringsSep
+    literalExpression
+    makeBinPath
+    mapAttrsToList
+    mdDoc
+    mkEnableOption
+    mkIf
+    mkMerge
+    mkOption
+    mkPackageOption
+    optionalAttrs
+    optionalString
+    types
+    ;
+
   cfg = config.services.node-red;
   defaultUser = "node-red";
   finalPackage = if cfg.withNpmAndGcc then node-red_withNpmAndGcc else cfg.package;
@@ -12,19 +26,19 @@ let
   ''
     mkdir -p $out/bin
     makeWrapper ${pkgs.nodePackages.node-red}/bin/node-red $out/bin/node-red \
-      --set PATH '${lib.makeBinPath [ pkgs.nodePackages.npm pkgs.gcc ]}:$PATH' \
+      --set PATH '${makeBinPath [ pkgs.nodePackages.npm pkgs.gcc ]}:$PATH' \
   '';
 in
 {
   options.services.node-red = {
-    enable = mkEnableOption (lib.mdDoc "the Node-RED service");
+    enable = mkEnableOption (mdDoc "the Node-RED service");
 
     package = mkPackageOption pkgs [ "nodePackages" "node-red" ] { };
 
     openFirewall = mkOption {
       type = types.bool;
       default = false;
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Open ports in the firewall for the server.
       '';
     };
@@ -32,7 +46,7 @@ in
     withNpmAndGcc = mkOption {
       type = types.bool;
       default = false;
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Give Node-RED access to NPM and GCC at runtime, so 'Nodes' can be
         downloaded and managed imperatively via the 'Palette Manager'.
       '';
@@ -42,7 +56,7 @@ in
       type = types.path;
       default = "${cfg.package}/lib/node_modules/node-red/settings.js";
       defaultText = literalExpression ''"''${package}/lib/node_modules/node-red/settings.js"'';
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Path to the JavaScript configuration file.
         See <https://github.com/node-red/node-red/blob/master/packages/node_modules/node-red/settings.js>
         for a configuration example.
@@ -52,13 +66,13 @@ in
     port = mkOption {
       type = types.port;
       default = 1880;
-      description = lib.mdDoc "Listening port.";
+      description = mdDoc "Listening port.";
     };
 
     user = mkOption {
       type = types.str;
       default = defaultUser;
-      description = lib.mdDoc ''
+      description = mdDoc ''
         User under which Node-RED runs.If left as the default value this user
         will automatically be created on system activation, otherwise the
         sysadmin is responsible for ensuring the user exists.
@@ -68,7 +82,7 @@ in
     group = mkOption {
       type = types.str;
       default = defaultUser;
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Group under which Node-RED runs.If left as the default value this group
         will automatically be created on system activation, otherwise the
         sysadmin is responsible for ensuring the group exists.
@@ -78,7 +92,7 @@ in
     userDir = mkOption {
       type = types.path;
       default = "/var/lib/node-red";
-      description = lib.mdDoc ''
+      description = mdDoc ''
         The directory to store all user data, such as flow and credential files and all library data. If left
         as the default value this directory will automatically be created before the node-red service starts,
         otherwise the sysadmin is responsible for ensuring the directory exists with appropriate ownership
@@ -89,13 +103,13 @@ in
     safe = mkOption {
       type = types.bool;
       default = false;
-      description = lib.mdDoc "Whether to launch Node-RED in --safe mode.";
+      description = mdDoc "Whether to launch Node-RED in --safe mode.";
     };
 
     define = mkOption {
       type = types.attrs;
       default = {};
-      description = lib.mdDoc "List of settings.js overrides to pass via -D to Node-RED.";
+      description = mdDoc "List of settings.js overrides to pass via -D to Node-RED.";
       example = literalExpression ''
         {
           "logging.console.level" = "trace";
@@ -131,7 +145,7 @@ in
         {
           User = cfg.user;
           Group = cfg.group;
-          ExecStart = "${finalPackage}/bin/node-red ${pkgs.lib.optionalString cfg.safe "--safe"} --settings ${cfg.configFile} --port ${toString cfg.port} --userDir ${cfg.userDir} ${concatStringsSep " " (mapAttrsToList (name: value: "-D ${name}=${value}") cfg.define)}";
+          ExecStart = "${finalPackage}/bin/node-red ${pkgs.optionalString cfg.safe "--safe"} --settings ${cfg.configFile} --port ${toString cfg.port} --userDir ${cfg.userDir} ${concatStringsSep " " (mapAttrsToList (name: value: "-D ${name}=${value}") cfg.define)}";
           PrivateTmp = true;
           Restart = "always";
           WorkingDirectory = cfg.userDir;
