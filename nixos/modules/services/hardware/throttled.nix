7ac01b6df39fe566ec45a722add8ded893acdbd4
Philip Taron <philip.taron@gmail.com>
2024-03-04 20:03:21 -0800

    nixos/throttled: avoid with lib

--- nixos/modules/services/hardware/throttled.nix
+++ nixos/modules/services/hardware/throttled.nix
@@ -1,24 +1,33 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    mdDoc
+    mkEnableOption
+    mkIf
+    mkOption
+    types
+    versionAtLeast
+    ;
+
   cfg = config.services.throttled;
-in {
+in
+{
   options = {
     services.throttled = {
-      enable = mkEnableOption (lib.mdDoc "fix for Intel CPU throttling");
+      enable = mkEnableOption (mdDoc "fix for Intel CPU throttling");
 
       extraConfig = mkOption {
         type = types.str;
         default = "";
-        description = lib.mdDoc "Alternative configuration";
+        description = mdDoc "Alternative configuration";
       };
     };
   };
 
   config = mkIf cfg.enable {
     systemd.packages = [ pkgs.throttled ];
+
     # The upstream package has this in Install, but that's not enough, see the NixOS manual
     systemd.services.throttled.wantedBy = [ "multi-user.target" ];
 
@@ -28,6 +37,7 @@ in {
       else "${pkgs.throttled}/etc/throttled.conf";
 
     hardware.cpu.x86.msr.enable = true;
+
     # Kernel 5.9 spams warnings whenever userspace writes to CPU MSRs.
     # See https://github.com/erpalma/throttled/issues/215
     hardware.cpu.x86.msr.settings.allow-writes =
