121f6c6a806018bf7ea1c80804532ff2bd0c2ff1
Philip Taron <philip.taron@gmail.com>
2024-03-05 15:36:20 -0800

    Avoid `with lib;` at the top level in nixos/modules/virtualisation/digital-ocean-image.nix

--- nixos/modules/virtualisation/digital-ocean-image.nix
+++ nixos/modules/virtualisation/digital-ocean-image.nix
@@ -1,7 +1,13 @@
 { config, lib, pkgs, ... }:
 
-with lib;
 let
+  inherit (lib)
+    maintainers
+    mdDoc
+    mkOption
+    types
+    ;
+
   cfg = config.virtualisation.digitalOceanImage;
 in
 {
@@ -13,7 +19,7 @@ in
       type = with types; either (enum [ "auto" ]) int;
       default = "auto";
       example = 4096;
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Size of disk image. Unit is MB.
       '';
     };
@@ -21,7 +27,7 @@ in
     virtualisation.digitalOceanImage.configFile = mkOption {
       type = with types; nullOr path;
       default = null;
-      description = lib.mdDoc ''
+      description = mdDoc ''
         A path to a configuration file which will be placed at
         `/etc/nixos/configuration.nix` and be used when switching
         to a new configuration. If set to `null`, a default
@@ -34,7 +40,7 @@ in
       type = types.enum [ "gzip" "bzip2" ];
       default = "gzip";
       example = "bzip2";
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Disk image compression method. Choose bzip2 to generate smaller images that
         take longer to generate but will consume less metered storage space on your
         Digital Ocean account.
