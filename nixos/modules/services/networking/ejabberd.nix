251b2ae0ebe0a2f50f656f91b2c8a8de44e04d6b
Philip Taron <philip.taron@gmail.com>
2024-03-05 10:32:25 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/networking/ejabberd.nix

--- nixos/modules/services/networking/ejabberd.nix
+++ nixos/modules/services/networking/ejabberd.nix
@@ -1,8 +1,18 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    escapeShellArgs
+    literalExpression
+    mdDoc
+    mkIf
+    mkOption
+    mkPackageOption
+    optional
+    optionalAttrs
+    optionalString
+    types
+    ;
 
   cfg = config.services.ejabberd;
 
@@ -13,7 +23,7 @@ let
 
   ectl = ''${cfg.package}/bin/ejabberdctl ${optionalString (cfg.configFile != null) "--config ${cfg.configFile}"} --ctl-config "${ctlcfg}" --spool "${cfg.spoolDir}" --logs "${cfg.logsDir}"'';
 
-  dumps = lib.escapeShellArgs cfg.loadDumps;
+  dumps = escapeShellArgs cfg.loadDumps;
 
 in {
 
@@ -26,7 +36,7 @@ in {
       enable = mkOption {
         type = types.bool;
         default = false;
-        description = lib.mdDoc "Whether to enable ejabberd server";
+        description = mdDoc "Whether to enable ejabberd server";
       };
 
       package = mkPackageOption pkgs "ejabberd" { };
@@ -34,50 +44,50 @@ in {
       user = mkOption {
         type = types.str;
         default = "ejabberd";
-        description = lib.mdDoc "User under which ejabberd is ran";
+        description = mdDoc "User under which ejabberd is ran";
       };
 
       group = mkOption {
         type = types.str;
         default = "ejabberd";
-        description = lib.mdDoc "Group under which ejabberd is ran";
+        description = mdDoc "Group under which ejabberd is ran";
       };
 
       spoolDir = mkOption {
         type = types.path;
         default = "/var/lib/ejabberd";
-        description = lib.mdDoc "Location of the spooldir of ejabberd";
+        description = mdDoc "Location of the spooldir of ejabberd";
       };
 
       logsDir = mkOption {
         type = types.path;
         default = "/var/log/ejabberd";
-        description = lib.mdDoc "Location of the logfile directory of ejabberd";
+        description = mdDoc "Location of the logfile directory of ejabberd";
       };
 
       configFile = mkOption {
         type = types.nullOr types.path;
-        description = lib.mdDoc "Configuration file for ejabberd in YAML format";
+        description = mdDoc "Configuration file for ejabberd in YAML format";
         default = null;
       };
 
       ctlConfig = mkOption {
         type = types.lines;
         default = "";
-        description = lib.mdDoc "Configuration of ejabberdctl";
+        description = mdDoc "Configuration of ejabberdctl";
       };
 
       loadDumps = mkOption {
         type = types.listOf types.path;
         default = [];
-        description = lib.mdDoc "Configuration dumps that should be loaded on the first startup";
+        description = mdDoc "Configuration dumps that should be loaded on the first startup";
         example = literalExpression "[ ./myejabberd.dump ]";
       };
 
       imagemagick = mkOption {
         type = types.bool;
         default = false;
-        description = lib.mdDoc "Add ImageMagick to server's path; allows for image thumbnailing";
+        description = mdDoc "Add ImageMagick to server's path; allows for image thumbnailing";
       };
     };
 
@@ -106,7 +116,7 @@ in {
       description = "ejabberd server";
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
-      path = [ pkgs.findutils pkgs.coreutils ] ++ lib.optional cfg.imagemagick pkgs.imagemagick;
+      path = [ pkgs.findutils pkgs.coreutils ] ++ optional cfg.imagemagick pkgs.imagemagick;
 
       serviceConfig = {
         User = cfg.user;
