edb6bfa355d3c33c8884fbf1e8e28601537639e5
Philip Taron <philip.taron@gmail.com>
2024-03-05 13:33:25 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/web-servers/lighttpd/collectd.nix

--- nixos/modules/services/web-servers/lighttpd/collectd.nix
+++ nixos/modules/services/web-servers/lighttpd/collectd.nix
@@ -1,8 +1,16 @@
 { config, lib, options, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    literalMD
+    mdDoc
+    mkEnableOption
+    mkIf
+    mkOption
+    overrideDerivation
+    types
+    ;
+
   cfg = config.services.lighttpd.collectd;
   opt = options.services.lighttpd.collectd;
 
@@ -25,7 +33,7 @@ in
 
   options.services.lighttpd.collectd = {
 
-    enable = mkEnableOption (lib.mdDoc "collectd subservice accessible at http://yourserver/collectd");
+    enable = mkEnableOption (mdDoc "collectd subservice accessible at http://yourserver/collectd");
 
     collectionCgi = mkOption {
       type = types.path;
@@ -33,7 +41,7 @@ in
       defaultText = literalMD ''
         `config.${options.services.collectd.package}` configured for lighttpd
       '';
-      description = lib.mdDoc ''
+      description = mdDoc ''
         Path to collection.cgi script from (collectd sources)/contrib/collection.cgi
         This option allows to use a customized version
       '';
