133bfd037b7d1ae2acab8836d00e50816db080df
Philip Taron <philip.taron@gmail.com>
2024-03-05 14:02:34 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/x11/window-managers/sawfish.nix

--- nixos/modules/services/x11/window-managers/sawfish.nix
+++ nixos/modules/services/x11/window-managers/sawfish.nix
@@ -1,14 +1,19 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    mdDoc
+    mkEnableOption
+    mkIf
+    singleton
+    ;
+
   cfg = config.services.xserver.windowManager.sawfish;
 in
 {
   ###### interface
   options = {
-    services.xserver.windowManager.sawfish.enable = mkEnableOption (lib.mdDoc "sawfish");
+    services.xserver.windowManager.sawfish.enable = mkEnableOption (mdDoc "sawfish");
   };
 
   ###### implementation
