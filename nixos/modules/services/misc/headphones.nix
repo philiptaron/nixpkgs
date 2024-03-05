e87c19fbfa47d8cf535a1f301aabdeb880912e06
Philip Taron <philip.taron@gmail.com>
2024-03-04 21:11:05 -0800

    nixos/headphones: avoid with lib

--- nixos/modules/services/misc/headphones.nix
+++ nixos/modules/services/misc/headphones.nix
@@ -1,12 +1,19 @@
 { config, lib, options, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    literalExpression
+    mdDoc
+    mkIf
+    mkOption
+    optionalAttrs
+    types
+    ;
 
   name = "headphones";
 
   cfg = config.services.headphones;
+
   opt = options.services.headphones;
 
 in
@@ -20,38 +27,38 @@ in
       enable = mkOption {
         type = types.bool;
         default = false;
-        description = lib.mdDoc "Whether to enable the headphones server.";
+        description = mdDoc "Whether to enable the headphones server.";
       };
       dataDir = mkOption {
         type = types.path;
         default = "/var/lib/${name}";
-        description = lib.mdDoc "Path where to store data files.";
+        description = mdDoc "Path where to store data files.";
       };
       configFile = mkOption {
         type = types.path;
         default = "${cfg.dataDir}/config.ini";
         defaultText = literalExpression ''"''${config.${opt.dataDir}}/config.ini"'';
-        description = lib.mdDoc "Path to config file.";
+        description = mdDoc "Path to config file.";
       };
       host = mkOption {
         type = types.str;
         default = "localhost";
-        description = lib.mdDoc "Host to listen on.";
+        description = mdDoc "Host to listen on.";
       };
       port = mkOption {
         type = types.ints.u16;
         default = 8181;
-        description = lib.mdDoc "Port to bind to.";
+        description = mdDoc "Port to bind to.";
       };
       user = mkOption {
         type = types.str;
         default = name;
-        description = lib.mdDoc "User to run the service as";
+        description = mdDoc "User to run the service as";
       };
       group = mkOption {
         type = types.str;
         default = name;
-        description = lib.mdDoc "Group to run the service as";
+        description = mdDoc "Group to run the service as";
       };
     };
   };
