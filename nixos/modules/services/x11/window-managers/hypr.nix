10840edf9cf8298dff7b4dcd35aadf451fb5e8a6
Philip Taron <philip.taron@gmail.com>
2024-03-05 14:01:12 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/x11/window-managers/hypr.nix

--- nixos/modules/services/x11/window-managers/hypr.nix
+++ nixos/modules/services/x11/window-managers/hypr.nix
@@ -1,14 +1,19 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    mdDoc
+    mkEnableOption
+    mkIf
+    singleton
+    ;
+
   cfg = config.services.xserver.windowManager.hypr;
 in
 {
   ###### interface
   options = {
-    services.xserver.windowManager.hypr.enable = mkEnableOption (lib.mdDoc "hypr");
+    services.xserver.windowManager.hypr.enable = mkEnableOption (mdDoc "hypr");
   };
 
   ###### implementation
