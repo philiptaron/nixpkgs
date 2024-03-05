733d942661244eb1f145c9ed62b57a63af68a4cf
Philip Taron <philip.taron@gmail.com>
2024-03-05 15:06:50 -0800

    Avoid `with lib;` at the top level in nixos/modules/tasks/filesystems/ntfs.nix

--- nixos/modules/tasks/filesystems/ntfs.nix
+++ nixos/modules/tasks/filesystems/ntfs.nix
@@ -1,6 +1,10 @@
 { config, lib, pkgs, ... }:
 
-with lib;
+let
+  inherit (lib)
+    mkIf
+    ;
+in
 
 {
   config = mkIf (config.boot.supportedFilesystems.ntfs or config.boot.supportedFilesystems.ntfs-3g or false) {
