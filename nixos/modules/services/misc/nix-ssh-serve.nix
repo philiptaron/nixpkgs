737813a5faf5806710509057016513446c6e04d3
Philip Taron <philip.taron@gmail.com>
2024-03-04 21:28:36 -0800

    nixos/nix-ssh-serve: avoid with lib

--- nixos/modules/services/misc/nix-ssh-serve.nix
+++ nixos/modules/services/misc/nix-ssh-serve.nix
@@ -1,12 +1,21 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-let cfg = config.nix.sshServe;
-    command =
+let
+  inherit (lib)
+    mdDoc
+    mkIf
+    mkOption
+    optionalString
+    types
+    ;
+
+  cfg = config.nix.sshServe;
+  command =
       if cfg.protocol == "ssh"
-        then "nix-store --serve ${lib.optionalString cfg.write "--write"}"
+        then "nix-store --serve ${optionalString cfg.write "--write"}"
       else "nix-daemon --stdio";
-in {
+in
+{
   options = {
 
     nix.sshServe = {
@@ -14,26 +23,26 @@ in {
       enable = mkOption {
         type = types.bool;
         default = false;
-        description = lib.mdDoc "Whether to enable serving the Nix store as a remote store via SSH.";
+        description = mdDoc "Whether to enable serving the Nix store as a remote store via SSH.";
       };
 
       write = mkOption {
         type = types.bool;
         default = false;
-        description = lib.mdDoc "Whether to enable writing to the Nix store as a remote store via SSH. Note: the sshServe user is named nix-ssh and is not a trusted-user. nix-ssh should be added to the {option}`nix.settings.trusted-users` option in most use cases, such as allowing remote building of derivations.";
+        description = mdDoc "Whether to enable writing to the Nix store as a remote store via SSH. Note: the sshServe user is named nix-ssh and is not a trusted-user. nix-ssh should be added to the {option}`nix.settings.trusted-users` option in most use cases, such as allowing remote building of derivations.";
       };
 
       keys = mkOption {
         type = types.listOf types.str;
         default = [];
         example = [ "ssh-dss AAAAB3NzaC1k... alice@example.org" ];
-        description = lib.mdDoc "A list of SSH public keys allowed to access the binary cache via SSH.";
+        description = mdDoc "A list of SSH public keys allowed to access the binary cache via SSH.";
       };
 
       protocol = mkOption {
         type = types.enum [ "ssh" "ssh-ng" ];
         default = "ssh";
-        description = lib.mdDoc "The specific Nix-over-SSH protocol to use.";
+        description = mdDoc "The specific Nix-over-SSH protocol to use.";
       };
 
     };
