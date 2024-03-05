51c33885d4c030a4953828e99bfb74b9fa255918
Philip Taron <philip.taron@gmail.com>
2024-03-05 13:46:14 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/x11/desktop-managers/xterm.nix

--- nixos/modules/services/x11/desktop-managers/xterm.nix
+++ nixos/modules/services/x11/desktop-managers/xterm.nix
@@ -1,8 +1,15 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    literalExpression
+    mdDoc
+    mkIf
+    mkOption
+    singleton
+    types
+    versionOlder
+    ;
 
   cfg = config.services.xserver.desktopManager.xterm;
   xSessionEnabled = config.services.xserver.enable;
@@ -16,7 +23,7 @@ in
       type = types.bool;
       default = versionOlder config.system.stateVersion "19.09" && xSessionEnabled;
       defaultText = literalExpression ''versionOlder config.system.stateVersion "19.09" && config.services.xserver.enable;'';
-      description = lib.mdDoc "Enable a xterm terminal as a desktop manager.";
+      description = mdDoc "Enable a xterm terminal as a desktop manager.";
     };
 
   };
