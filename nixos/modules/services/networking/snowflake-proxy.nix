3cc56e227cdc0eb395c219f2936214a6c7d93fd9
Philip Taron <philip.taron@gmail.com>
2024-03-05 11:38:30 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/networking/snowflake-proxy.nix

--- nixos/modules/services/networking/snowflake-proxy.nix
+++ nixos/modules/services/networking/snowflake-proxy.nix
@@ -1,35 +1,44 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    concatStringsSep
+    maintainers
+    mdDoc
+    mkEnableOption
+    mkIf
+    mkOption
+    optional
+    types
+    ;
+
   cfg = config.services.snowflake-proxy;
 in
 {
   options = {
     services.snowflake-proxy = {
-      enable = mkEnableOption (lib.mdDoc "snowflake-proxy, a system to defeat internet censorship");
+      enable = mkEnableOption (mdDoc "snowflake-proxy, a system to defeat internet censorship");
 
       broker = mkOption {
-        description = lib.mdDoc "Broker URL (default \"https://snowflake-broker.torproject.net/\")";
+        description = mdDoc "Broker URL (default \"https://snowflake-broker.torproject.net/\")";
         type = with types; nullOr str;
         default = null;
       };
 
       capacity = mkOption {
-        description = lib.mdDoc "Limits the amount of maximum concurrent clients allowed.";
+        description = mdDoc "Limits the amount of maximum concurrent clients allowed.";
         type = with types; nullOr int;
         default = null;
       };
 
       relay = mkOption {
-        description = lib.mdDoc "websocket relay URL (default \"wss://snowflake.bamsoftware.com/\")";
+        description = mdDoc "websocket relay URL (default \"wss://snowflake.bamsoftware.com/\")";
         type = with types; nullOr str;
         default = null;
       };
 
       stun = mkOption {
-        description = lib.mdDoc "STUN broker URL (default \"stun:stun.stunprotocol.org:3478\")";
+        description = mdDoc "STUN broker URL (default \"stun:stun.stunprotocol.org:3478\")";
         type = with types; nullOr str;
         default = null;
       };
