459ad18f085a2db4b9d36114c2ba9316adc5b257
Philip Taron <philip.taron@gmail.com>
2024-03-05 13:32:09 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/web-servers/jboss/default.nix

--- nixos/modules/services/web-servers/jboss/default.nix
+++ nixos/modules/services/web-servers/jboss/default.nix
@@ -1,8 +1,12 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    mdDoc
+    mkIf
+    mkOption
+    types
+    ;
 
   cfg = config.services.jboss;
 
@@ -26,49 +30,49 @@ in
       enable = mkOption {
         type = types.bool;
         default = false;
-        description = lib.mdDoc "Whether to enable JBoss. WARNING : this package is outdated and is known to have vulnerabilities.";
+        description = mdDoc "Whether to enable JBoss. WARNING : this package is outdated and is known to have vulnerabilities.";
       };
 
       tempDir = mkOption {
         default = "/tmp";
         type = types.str;
-        description = lib.mdDoc "Location where JBoss stores its temp files";
+        description = mdDoc "Location where JBoss stores its temp files";
       };
 
       logDir = mkOption {
         default = "/var/log/jboss";
         type = types.str;
-        description = lib.mdDoc "Location of the logfile directory of JBoss";
+        description = mdDoc "Location of the logfile directory of JBoss";
       };
 
       serverDir = mkOption {
-        description = lib.mdDoc "Location of the server instance files";
+        description = mdDoc "Location of the server instance files";
         default = "/var/jboss/server";
         type = types.str;
       };
 
       deployDir = mkOption {
-        description = lib.mdDoc "Location of the deployment files";
+        description = mdDoc "Location of the deployment files";
         default = "/nix/var/nix/profiles/default/server/default/deploy/";
         type = types.str;
       };
 
       libUrl = mkOption {
         default = "file:///nix/var/nix/profiles/default/server/default/lib";
-        description = lib.mdDoc "Location where the shared library JARs are stored";
+        description = mdDoc "Location where the shared library JARs are stored";
         type = types.str;
       };
 
       user = mkOption {
         default = "nobody";
-        description = lib.mdDoc "User account under which jboss runs.";
+        description = mdDoc "User account under which jboss runs.";
         type = types.str;
       };
 
       useJK = mkOption {
         type = types.bool;
         default = false;
-        description = lib.mdDoc "Whether to use to connector to the Apache HTTP server";
+        description = mdDoc "Whether to use to connector to the Apache HTTP server";
       };
 
     };
