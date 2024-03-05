ec3a429644f34bd6f83a3fa7b1b53b0741804eec
Philip Taron <philip.taron@gmail.com>
2024-03-05 14:02:45 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/x11/window-managers/windowmaker.nix

--- nixos/modules/services/x11/window-managers/windowmaker.nix
+++ nixos/modules/services/x11/window-managers/windowmaker.nix
@@ -1,14 +1,19 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
+  inherit (lib)
+    mdDoc
+    mkEnableOption
+    mkIf
+    singleton
+    ;
+
   cfg = config.services.xserver.windowManager.windowmaker;
 in
 {
   ###### interface
   options = {
-    services.xserver.windowManager.windowmaker.enable = mkEnableOption (lib.mdDoc "windowmaker");
+    services.xserver.windowManager.windowmaker.enable = mkEnableOption (mdDoc "windowmaker");
   };
 
   ###### implementation
