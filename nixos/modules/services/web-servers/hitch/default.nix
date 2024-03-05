d518190b9b835973bf916962b33f44b75e16e234
Philip Taron <philip.taron@gmail.com>
2024-03-05 13:32:03 -0800

    Avoid `with lib;` at the top level in nixos/modules/services/web-servers/hitch/default.nix

--- nixos/modules/services/web-servers/hitch/default.nix
+++ nixos/modules/services/web-servers/hitch/default.nix
@@ -1,7 +1,22 @@
 { config, lib, pkgs, ...}:
+
 let
+  inherit (lib)
+    concatMapStrings
+    concatStringsSep
+    mdDoc
+    mkEnableOption
+    mkIf
+    mkOption
+    optionalString
+    toList
+    types
+    ;
+
   cfg = config.services.hitch;
-  ocspDir = lib.optionalString cfg.ocsp-stapling.enabled "/var/cache/hitch/ocsp";
+
+  ocspDir = optionalString cfg.ocsp-stapling.enabled "/var/cache/hitch/ocsp";
+
   hitchConfig = with lib; pkgs.writeText "hitch.conf" (concatStringsSep "\n" [
     ("backend = \"${cfg.backend}\"")
     (concatMapStrings (s: "frontend = \"${s}\"\n") cfg.frontend)
@@ -13,15 +28,14 @@ let
     cfg.extraConfig
   ]);
 in
-with lib;
 {
   options = {
     services.hitch = {
-      enable = mkEnableOption (lib.mdDoc "Hitch Server");
+      enable = mkEnableOption (mdDoc "Hitch Server");
 
       backend = mkOption {
         type = types.str;
-        description = lib.mdDoc ''
+        description = mdDoc ''
           The host and port Hitch connects to when receiving
           a connection in the form [HOST]:PORT
         '';
@@ -30,13 +44,13 @@ with lib;
       ciphers = mkOption {
         type = types.str;
         default = "EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH";
-        description = lib.mdDoc "The list of ciphers to use";
+        description = mdDoc "The list of ciphers to use";
       };
 
       frontend = mkOption {
         type = types.either types.str (types.listOf types.str);
         default = "[127.0.0.1]:443";
-        description = lib.mdDoc ''
+        description = mdDoc ''
           The port and interface of the listen endpoint in the
           form [HOST]:PORT[+CERT].
         '';
@@ -46,33 +60,33 @@ with lib;
       pem-files = mkOption {
         type = types.listOf types.path;
         default = [];
-        description = lib.mdDoc "PEM files to use";
+        description = mdDoc "PEM files to use";
       };
 
       ocsp-stapling = {
         enabled = mkOption {
           type = types.bool;
           default = true;
-          description = lib.mdDoc "Whether to enable OCSP Stapling";
+          description = mdDoc "Whether to enable OCSP Stapling";
         };
       };
 
       user = mkOption {
         type = types.str;
         default = "hitch";
-        description = lib.mdDoc "The user to run as";
+        description = mdDoc "The user to run as";
       };
 
       group = mkOption {
         type = types.str;
         default = "hitch";
-        description = lib.mdDoc "The group to run as";
+        description = mdDoc "The group to run as";
       };
 
       extraConfig = mkOption {
         type = types.lines;
         default = "";
-        description = lib.mdDoc "Additional configuration lines";
+        description = mdDoc "Additional configuration lines";
       };
     };
 
