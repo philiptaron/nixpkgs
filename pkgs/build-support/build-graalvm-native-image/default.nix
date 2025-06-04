9411ce5808178a3d5f46da0cc600e70cb1bc1fba
Philip Taron <philip.taron@gmail.com>
2025-06-04 10:21:32 -0700

    buildGraalvmNativeImage: simplify further

--- pkgs/build-support/build-graalvm-native-image/default.nix
+++ pkgs/build-support/build-graalvm-native-image/default.nix
@@ -11,34 +11,22 @@ lib.extendMkDerivation {
   constructDrv = stdenv.mkDerivation;
 
   excludeDrvArgNames = [
-    "LC_ALL"
-    "executable"
     "extraNativeImageBuildArgs"
     "graalvmDrv"
-    "jar"
   ];
 
   extendDrvArgs =
     finalAttrs:
     {
-      src,
-      jar ? src,
-      executable ? null,
+      dontUnpack ? true,
+      strictDeps ? true,
+      __structuredAttrs ? true,
 
       # The GraalVM derivation to use
       graalvmDrv ? graalvmPackages.graalvm-ce,
 
-      # Default native-image arguments. You probably don't want to set this,
-      # except in special cases. In most cases, use extraNativeBuildArgs instead
-      nativeImageBuildArgs ? null,
-
       # Extra arguments to be passed to the native-image
       extraNativeImageBuildArgs ? [ ],
-
-      # XMX size of GraalVM during build
-      graalvmXmx ? "-J-Xmx6g",
-
-      LC_ALL ? "en_US.UTF-8",
 
       env ? { },
       meta ? { },
@@ -46,34 +34,17 @@ lib.extendMkDerivation {
       ...
     }@args:
     let
-      executable' = if executable == null then finalAttrs.meta.mainProgram else executable;
+      executable = finalAttrs.meta.mainProgram;
 
-      nativeImageBuildArgs' =
-        if nativeImageBuildArgs != null then
-          nativeImageBuildArgs
-        else
-          [
-            (lib.optionalString stdenv.hostPlatform.isDarwin "-H:-CheckToolchain")
-            (lib.optionalString (
-              stdenv.hostPlatform.isLinux && stdenv.hostPlatform.isAarch64
-            ) "-H:PageSize=64K")
-            "-H:Name=${executable'}"
-            "-march=compatibility"
-            "--verbose"
-          ];
+      # XMX size of GraalVM during build
+      graalvmXmx = "-J-Xmx6g";
     in
-
     {
-      strictDeps = args.strictDeps or true;
-      __structuredAttrs = args.__structuredAttrs or true;
-
       env = {
-        inherit LC_ALL;
+        LC_ALL = "en_US.UTF-8";
       } // env;
 
-      inherit jar;
-
-      dontUnpack = args.dontUnpack or (jar == finalAttrs.src);
+      inherit dontUnpack strictDeps __structuredAttrs;
 
       nativeBuildInputs = (args.nativeBuildInputs or [ ]) ++ [
         graalvmDrv
@@ -81,13 +52,28 @@ lib.extendMkDerivation {
         removeReferencesTo
       ];
 
-      nativeImageBuildArgs = nativeImageBuildArgs' ++ extraNativeImageBuildArgs ++ [ graalvmXmx ];
+      # Default native-image arguments. You probably don't want to set this,
+      # except in special cases. In most cases, use extraNativeBuildArgs instead
+      nativeImageBuildArgs =
+        args.nativeImageBuildArgs or (
+          [
+            (lib.optionalString stdenv.hostPlatform.isDarwin "-H:-CheckToolchain")
+            (lib.optionalString (
+              stdenv.hostPlatform.isLinux && stdenv.hostPlatform.isAarch64
+            ) "-H:PageSize=64K")
+            "-H:Name=${executable}"
+            "-march=compatibility"
+            "--verbose"
+          ]
+          ++ extraNativeImageBuildArgs
+          ++ [ graalvmXmx ]
+        );
 
       buildPhase =
         args.buildPhase or ''
           runHook preBuild
 
-          native-image -jar "$jar" ''${nativeImageBuildArgs[@]}
+          native-image -jar "$src" ''${nativeImageBuildArgs[@]}
 
           runHook postBuild
         '';
@@ -96,13 +82,13 @@ lib.extendMkDerivation {
         args.installPhase or ''
           runHook preInstall
 
-          install -Dm755 ${executable'} -t $out/bin
+          install -Dm755 ${executable} -t $out/bin
 
           runHook postInstall
         '';
 
       postInstall = ''
-        remove-references-to -t ${graalvmDrv} $out/bin/${executable'}
+        remove-references-to -t ${graalvmDrv} $out/bin/${executable}
         ${args.postInstall or ""}
       '';
 
@@ -112,14 +98,9 @@ lib.extendMkDerivation {
         inherit graalvmDrv;
       } // passthru;
 
-      meta =
-        {
-          # default to graalvm's platforms
-          inherit (graalvmDrv.meta) platforms;
-        }
-        // lib.optionalAttrs (executable != null) {
-          mainProgram = executable;
-        }
-        // meta;
+      meta = {
+        # default to graalvm's platforms
+        inherit (graalvmDrv.meta) platforms;
+      } // meta;
     };
 }
